3016ede3c60a6485091c09cff6a5950781ea7655
Peter Anderson <pete.anderson@gmail.com>
2024-11-07 09:01:54 +0100

    --add: uploadthing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { generateUploadButton } from "@uploadthing/react";
+
+export const UploadButton = generateUploadButton();
+
 export default function HomePage() {
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <UploadButton
+        endpoint="imageUploader"
+        onClientUploadComplete={(res) => {
+          // Do something with the response
+          console.log("Files: ", res);
+          alert("Upload Completed");
+        }}
+        onUploadError={(error) => {
+          // Do something with the error.
+          alert(`ERROR! ${error.message}`);
+        }}
+      />
     </div>
   );
 }
